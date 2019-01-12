03eb198a241e8542b9ba1fde28c832ccfe2b8ed6
korepetycje2019 <pisulamiroslaw@gmail.com>
2019-01-12 15:53:02 +0100

    początek formularza

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Formularz</title>
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    Witaj
-  </body>
+</head>
+<body>
+  <form action="" method="POST">
+   <p>Podaj imię:</p>
+   <input type="text" name="imie"></br></br>
+   <input type="submit" name="button" value="Wyślij"></br></br>
+  </form>
+<?php
+if (isset($_POST['imie'])){
+  $imie = $_POST['imie'];
+  echo '<strong>Witaj '.$imie.' </strong>';
+}
+
+ ?>
+</body>
 </html>
